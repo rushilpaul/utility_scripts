6575c32d16454c7611c68d791ffc4790be31ee78
Rushil Paul <rushipau@cisco.com>
2019-01-05 22:12:18 +0530

    Improved and renamed udemy course downloader

--- udemy_course_downloader.py
+++ udemy_course_downloader.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import sys, json, os, traceback
+import sys, json, os, traceback, string
 from urlparse import urlparse, parse_qs
 import requests as R
 
@@ -9,12 +9,33 @@ if len(sys.argv) <= 1:
 	sys.exit(1)
 
 courseId = sys.argv[1]
-print 'Course to be downloaded', courseId
+apiResponseDataDir = 'API_Response_data_files'
 
-bearerToken = "Bearer ldNCv9Z2WZ7mEr2YpIo7Dxx9PJxop27hDyPf9cPA"
-headers = { 'Authorization' : bearerToken, 'Cookie' : '__udmy_2_v57r=7ff20f2adf074b5c9072ae9d441d0ac0; ufb_acc="E0AecllSVw=="; _ga=GA1.2.1652209469.1545915786; _gid=GA1.2.1933479833.1546591936; intercom-session-sehj53dd=cncyZWlYbnlpaGhPaHFPNHFFMDhGdHVCQ0RrcFpPTWx5U3VESk9FKzBrQ1JBS0xyM1FaMW9ad0xJczRUWDc3cS0ta3kxM0JEcWVjaGZIS2JEUnZ5bUt1dz09--572e37ba14fcf5f8369c92b9837760ea86f06fc4' }
+cookieHeader = { 'Cookie' : '__udmy_2_v57r=7ff20f2adf074b5c9072ae9d441d0ac0; ufb_acc="E0AecllSVw=="; _ga=GA1.2.1652209469.1545915786; _gid=GA1.2.1933479833.1546591936; intercom-session-sehj53dd=cncyZWlYbnlpaGhPaHFPNHFFMDhGdHVCQ0RrcFpPTWx5U3VESk9FKzBrQ1JBS0xyM1FaMW9ad0xJczRUWDc3cS0ta3kxM0JEcWVjaGZIS2JEUnZ5bUt1dz09--572e37ba14fcf5f8369c92b9837760ea86f06fc4' }
+bearerTokenHeader = { 'Authorization' : 'Bearer ldNCv9Z2WZ7mEr2YpIo7Dxx9PJxop27hDyPf9cPA' }
 
-def downloadFile(url, fileName, depth):
+def formatFileName(fName):
+    """Take a string and return a valid filename constructed from the string.
+		Uses a whitelist approach: any characters not present in valid_chars are
+		removed. Also spaces are replaced with underscores.
+ 
+		Note: this method may produce invalid filenames such as ``, `.` or `..`
+		When I use this method I prepend a date string like '2009_01_15_19_46_32_'
+		and append a file extension like '.txt', so I avoid the potential of using
+		an invalid filename.
+	"""
+    invalid_chars = "/\\:"
+    finalName = ""
+    for i in fName:
+    	if i == ' ':
+    		finalName += '-'
+    	elif i in invalid_chars:
+    		finalName += ' '
+    	else:
+    		finalName += i
+    return finalName
+
+def downloadFile(url, fileName, headers, depth):
 	
 	prefix = '\t' * depth
 
@@ -24,9 +45,11 @@ def downloadFile(url, fileName, depth):
 
 	resp = R.get(url, headers = headers)
 	if not resp.ok:
-		print prefix + 'Could not download file {0}. Status code {1}'.format(fileName, resp.status_code)
-		return
+		print prefix + 'Could not download file "{0}" at "{1}". Status code {2}'.format(fileName, url, resp.status_code)
+		print resp.content
+		return False
 	open(fileName, 'wb').write(resp.content)
+	return True
 
 
 # lectureNumber is used to order supplement assets within a chapter
@@ -34,7 +57,7 @@ def downloadSupplementAssets(courseId, lectureId, lectureNumber, assetId, chapte
 
 	assetUrl = "https://cisco.udemy.com/api-2.0/users/me/subscribed-courses/{0}/lectures/{1}/supplementary-assets/{2}?fields[asset]=download_urls".format(courseId, lectureId, assetId)
 
-	resp = R.get(assetUrl, headers = headers)
+	resp = R.get(assetUrl, headers = bearerTokenHeader)
 	if not resp.ok:
 		print '\t\tCould not retrieve supplementary asset details for asset', assetId
 		return
@@ -54,8 +77,8 @@ def downloadSupplementAssets(courseId, lectureId, lectureNumber, assetId, chapte
 			pass
 
 		print '\t\tDownloading a supplementary file:', fileName
-		downloadFile(fileUrl, os.path.join(chapterName, str(lectureNumber) + '. ' + fileName), 2)
-		print '\t\tFinished downloading the supplementary file:', fileName
+		if downloadFile(fileUrl, os.path.join(chapterName, str(lectureNumber) + '. ' + formatFileName(fileName)), cookieHeader, 2):
+			print '\t\tFinished downloading the supplementary file:', fileName
 
 
 def downloadVideo(video, fileName):
@@ -67,8 +90,8 @@ def downloadVideo(video, fileName):
 		print '\tNot sure what file extension to apply to ' + fileName
 
 	print '\tStarted downloading video', fileName
-	downloadFile(url, fileName, 1)
-	print '\tFinished downloading video', fileName
+	if downloadFile(url, fileName, bearerTokenHeader, 1):
+		print '\tFinished downloading video', fileName
 
 
 
@@ -77,17 +100,17 @@ def downloadLecture(courseId, lectureId, lectureNumber, chapterName):
 
 	courseUrl = "https://cisco.udemy.com/api-2.0/users/me/subscribed-courses/{0}/lectures/{1}?fields%5Basset%5D=@min,download_urls,external_url,slide_urls,status,captions,thumbnail_url,time_estimation,stream_urls".format(courseId, lectureId)
 
-	resp = R.get(courseUrl, headers = headers)
+	resp = R.get(courseUrl, headers = bearerTokenHeader)
 	if not resp.ok:
 		print 'Could not get course details: ' + str(resp.status_code)
 		return
 
 	course = json.loads(resp.text)
 
-	open('API_response_lectureId_' + lectureId, 'wb').write(json.dumps(course, indent = 4))
+	open(os.path.join(apiResponseDataDir, 'API_response_lectureId_' + lectureId + '.json'), 'wb').write(json.dumps(course, indent = 4))
 
-	fileName = str(course['title'])
-	fileName = os.path.join(chapterName, str(lectureNumber) + '. ' + fileName)
+	fileName = course['title'].encode('utf-8')
+	fileName = os.path.join(chapterName, str(lectureNumber) + '. ' + formatFileName(fileName))
 
 	for video in course['asset']['stream_urls']['Video']:
 		if video['label'] == '720':
@@ -100,14 +123,14 @@ def start(courseId):
 
 	courseUrl = "https://cisco.udemy.com/api-2.0/courses/{0}/cached-subscriber-curriculum-items/?page_size=140000&fields[lecture]=@min,object_index,asset,supplementary_assets,sort_order&fields[quiz]=@min,object_index,title,sort_order&fields[practice]=@min,object_index,title,sort_order&fields[chapter]=@min,object_index,title,sort_order&fields[asset]=@min,asset_type".format(courseId)
 
-	resp = R.get(courseUrl, headers = headers)
+	resp = R.get(courseUrl, headers = bearerTokenHeader)
 	if not resp.ok:
 		print 'Could not get course details: ' + str(resp.status_code)
 		return None
 	
 	course = json.loads(resp.text)
 
-	open('API_response_courseId_' + courseId, 'wb').write(json.dumps(course, indent = 4))
+	open(os.path.join(apiResponseDataDir, 'API_response_courseId_' + courseId + '.json'), 'wb').write(json.dumps(course, indent = 4))
 
 	if(course['next'] != None):
 		print 'There are more sections that need to be fetched. This is not enough'
@@ -127,34 +150,36 @@ def start(courseId):
 		eClass = entity['_class'].lower()
 
 		if eClass == 'chapter':
-			chapterName = entity['title']
+			chapterName = entity['title'].encode('utf-8').strip()
+			lectureNumber = 1
 			print ''
 			try:
 				os.mkdir(chapterName)
 				print 'Directory for chapter {0} created'.format(chapterName)
-				lectureNumber = 1
 
 			except OSError as e:
 				print 'Directory {0} already exists. Continuing anyway'.format(chapterName)
 
 		elif eClass == 'lecture' and entity['asset']['asset_type'].lower() == 'video':
 			lectureId = entity['id']
-			try:
-				downloadLecture(courseId, str(lectureId), lectureNumber, chapterName)
-			except Exception as e:
-				print '\tSomething went wrong while downloading lecture {0}'.format(entity['title'])
-				print e
-				print traceback.print_exc()
+			downloadLecture(courseId, str(lectureId), lectureNumber, chapterName)
 
 			# Find and download supplementary assets
 			supAssets = entity['supplementary_assets']
 			if supAssets != None and len(supAssets) > 0:
 				for supAsset in supAssets:
-					if supAsset['_class'].lower() == 'asset':
-						downloadSupplementAssets(courseId, lectureId, lectureNumber, supAsset['id'], chapterName, supAsset['title'])
+					if supAsset['_class'].lower() == 'asset' and supAsset['asset_type'].lower() == 'file':
+						downloadSupplementAssets(courseId, lectureId, lectureNumber, supAsset['id'], chapterName, fileName = supAsset['title'])
 
 			lectureNumber += 1
 			print ''	# new line
 
 
-resp = start(courseId)
+try:
+	os.mkdir(apiResponseDataDir)
+except:
+	pass
+
+print 'Course to be downloaded', courseId
+start(courseId)
+
